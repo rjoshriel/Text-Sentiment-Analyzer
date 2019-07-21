0efe91f0cf341a7ad9a5f1de0ab7906989c91494
Elvin Cruz <50911342+elvincrz@users.noreply.github.com>
2019-07-21 18:59:11 +0800

    Add files via upload

---
+++ GUI_Reviews.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 20 18:19:06 2019
+
+@author: ELVIN
+"""
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sqlite3
+
+class Ui_viewRecords(object):
+    def setupUi(self, viewRecords):
+        viewRecords.setObjectName("viewRecords")
+        viewRecords.resize(470, 417)
+        self.tableWidget = QtWidgets.QTableWidget(viewRecords)
+        self.tableWidget.setGeometry(QtCore.QRect(10, 10, 341, 401))
+        self.tableWidget.setRowCount(99)
+        self.tableWidget.setColumnCount(3)
+        self.tableWidget.setObjectName("tableWidget")
+        self.load = QtWidgets.QPushButton(viewRecords)
+        self.load.setGeometry(QtCore.QRect(370, 330, 81, 31))
+        self.load.setObjectName("load")
+        self.load.clicked.connect(self.sorting)
+        self.back = QtWidgets.QPushButton(viewRecords)
+        self.back.setGeometry(QtCore.QRect(370, 370, 81, 31))
+        self.back.setObjectName("back")
+        self.back.clicked.connect(self.exitButton_clicked)
+        self.pushButton = QtWidgets.QPushButton(viewRecords)
+        self.pushButton.setGeometry(QtCore.QRect(370, 70, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.goodReview)
+        self.pushButton_2 = QtWidgets.QPushButton(viewRecords)
+        self.pushButton_2.setGeometry(QtCore.QRect(370, 100, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.clicked.connect(self.badReview)
+        self.label = QtWidgets.QLabel(viewRecords)
+        self.label.setGeometry(QtCore.QRect(380, 50, 51, 16))
+        self.label.setObjectName("label")
+
+        self.retranslateUi(viewRecords)
+        QtCore.QMetaObject.connectSlotsByName(viewRecords)
+        
+    def sorting(self):
+        conn = sqlite3.connect('products.db')
+        que = "SELECT * FROM Reviews ORDER BY rating desc"
+        res = conn.execute(que)
+        self.tableWidget.setRowCount(0)
+        
+        for row_number, row_data in enumerate(res):
+            self.tableWidget.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.tableWidget.setItem(row_number, column_number, QtWidgets.QTableWidgetItem(str(data)))
+                
+        conn.close()
+        
+    def goodReview(self):
+        conn = sqlite3.connect('products.db')
+        que = "SELECT * FROM Reviews WHERE rating >= 3 ORDER BY rating desc"
+        res = conn.execute(que)
+        self.tableWidget.setRowCount(0)
+        
+        for row_number, row_data in enumerate(res):
+            self.tableWidget.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.tableWidget.setItem(row_number, column_number, QtWidgets.QTableWidgetItem(str(data)))
+                
+        conn.close()
+        
+    def badReview(self):
+        conn = sqlite3.connect('products.db')
+        que = "SELECT * FROM Reviews WHERE rating < 3 ORDER BY rating desc"
+        res = conn.execute(que)
+        self.tableWidget.setRowCount(0)
+        
+        for row_number, row_data in enumerate(res):
+            self.tableWidget.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.tableWidget.setItem(row_number, column_number, QtWidgets.QTableWidgetItem(str(data)))
+                
+        conn.close()
+        
+    def exitButton_clicked(self):
+        viewRecords.close()
+        
+        
+
+    def retranslateUi(self, viewRecords):
+        _translate = QtCore.QCoreApplication.translate
+        viewRecords.setWindowTitle(_translate("viewRecords", "View Records"))
+        self.load.setText(_translate("viewRecords", "Load"))
+        self.back.setText(_translate("viewRecords", "Exit"))
+        self.pushButton.setText(_translate("viewRecords", "Good"))
+        self.pushButton_2.setText(_translate("viewRecords", "Bad"))
+        self.label.setText(_translate("viewRecords", "<html><head/><body><p><span style=\" font-weight:600;\">REVIEWS</span></p></body></html>"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    viewRecords = QtWidgets.QDialog()
+    ui = Ui_viewRecords()
+    ui.setupUi(viewRecords)
+    viewRecords.show()
+    sys.exit(app.exec_())
